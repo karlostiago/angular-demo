5073a3122796840b659477dd9bc45a5c17c7aab1
Carlos Tiago <carlostiagodesousa@gmail.com>
2021-04-30 10:45:32 -0300

    correcao do dirname do server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/angular-demo'));
 
 app.get('/*', function(request, response) {
-  response.sendFile(__dirname + '/dist/index.html');
+  response.sendFile(__dirname + '/dist/angular-demo/index.html');
 });
 
 app.listen(process.env.PORT || 4200);
