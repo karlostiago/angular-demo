69aa2dc08a5ed15c1765d6bc3d026dbe0f36064b
Carlos Tiago <carlostiagodesousa@gmail.com>
2021-04-30 11:20:56 -0300

    inclusao de itens para teste de performance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-demo';
+
+  itens: any = [];
+
+  ngOnInit() {
+    for (let i = 1; i < 100000; i++)
+    this.itens.push(`Item ${i}`);
+  }
 }
